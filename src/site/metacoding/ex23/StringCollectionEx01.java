08ea9e83b4b71f85df2c8bec698910e81e8b942c
stuuuu <woasmsshfl@naver.com>
2022-01-27 12:51:52 +0900

    2. StringBuffer 공부

---
+++ src/site/metacoding/ex23/StringCollectionEx01.java
@@ -0,0 +1,19 @@
+package site.metacoding.ex23;
+
+public class StringCollectionEx01 {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?");
+        sb.append(
+                "serviceKey=wJmmW29e3AEUjwLioQR22CpmqS645ep4S8TSlqtSbEsxvnkZFoNe7YG1weEWQHYZ229eNLidnI2Yt5EZ3Stv7g%3D%3D&");
+        sb.append("numOfRows=10&");
+        sb.append("pageNo=1&");
+        sb.append("dataType=JSON&");
+        sb.append("base_date=20220126&");
+        sb.append("base_time=1700&");
+        sb.append("nx=97&");
+        sb.append("ny=75");
+
+        System.out.println(sb.toString());
+    }
+}
